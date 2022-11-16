822a427d78fd02bff5c92b601745bc4e66545bab
GeoSauer <geoffrey.sauer89@gmail.com>
2022-11-16 15:26:53 -0700

    create new user route built and tested

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,14 +3,31 @@ const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
 
-describe('backend-express-template routes', () => {
+const mockUser = {
+  firstName: 'Mock',
+  lastName: 'User',
+  email: 'mock@example.com',
+  password: '123123',
+};
+
+describe('top secret routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  it('example test - delete me!', () => {
-    expect(1).toEqual(1);
-  });
+
   afterAll(() => {
     pool.end();
+  });
+
+  test('should create a new user', async () => {
+    const resp = await request(app).post('/api/v1/users').send(mockUser);
+    const { firstName, lastName, email } = mockUser;
+
+    expect(resp.body).toEqual({
+      id: expect.any(String),
+      firstName,
+      lastName,
+      email,
+    });
   });
 });
