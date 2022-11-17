7b7f24fc6df2c44f5e7d1ab80e13f4d68e5d95c9
GeoSauer <geoffrey.sauer89@gmail.com>
2022-11-16 17:03:52 -0700

    DELETE session route built and tested

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,6 +2,7 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const { UserService } = require('../lib/services/UserService.js');
 
 const mockUser = {
   firstName: 'Mock',
@@ -34,6 +35,17 @@ describe('top secret routes', () => {
       .post('/api/v1/users/sessions')
       .send({ email: 'mock@example.com', password: '123123' });
     expect(resp.status).toBe(200);
+  });
+
+  test('DELETE /api/v1/users/sessions should log out user', async () => {
+    const agent = request.agent(app);
+    const user = await UserService.create({ ...mockUser });
+    await agent
+      .post('/api/v1/users/sessions')
+      .send({ email: 'mock@example.com', password: '123123' });
+
+    const resp = await agent.delete('/api/v1/users/sessions');
+    expect(resp.status).toBe(204);
   });
 
   afterAll(() => {
